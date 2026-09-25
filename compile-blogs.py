d6a2716b372675c488d9d540ecb9f0bdcd5bf4ff
Gudhein3 <nikolasshevelkos@gmail.com>
2026-09-25 14:32:57 +0300

    Update posts

--- compile-blogs.py
+++ compile-blogs.py
@@ -3,20 +3,24 @@ from hashlib import sha1
 with open("blogpost_format.html.in", "r") as inp:
     blogpost_format = inp.read()
 
+posts = []
+
 with open("posts.txt", "r") as inp:
-    posts = []
     for post in inp.read().split("\n--------------------\n"):
-        title, date, body = post.split('\n', 2)
-        body = body.replace("\n", "<br>")
-        posts.append((title, date, body))
+        post = post.strip()
+        id_title, date, body = post.split('\n', 2)
+        id, title = id_title.split("|")
+        date, body = date.strip(), body.strip()
+        id, title = id.strip(), title.strip()
+        body = body.replace("\n", "<br>\n")
+        posts.append((title, id, date, body))
 
 bloglist = ""
-for title, date, body in posts[::-1]:
-    # Pray that it won't cause collision
-    hash = sha1(title.encode()).hexdigest()[:10]
-    with open(f"blog/post_{hash}.html", 'w') as out:
+for title, id, date, body in posts[::-1]:
+    # Hope that it won't cause collisions
+    with open(f"blog/post_{id}.html", 'w') as out:
         out.write(blogpost_format.replace("%title%", title).replace("%body%", body).replace("%date%", date))
-    bloglist += f"<a href=\"/blog/post_{hash}.html\">{title}</a><br>\n"
+    bloglist += f"<a href=\"/blog/post_{id}.html\">{title}</a><br>\n"
 
 with open("index.html.in", 'r') as inp:
     with open("index.html", 'w') as out:
